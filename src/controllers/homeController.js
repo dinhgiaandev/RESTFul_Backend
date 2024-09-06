81234eb974046ede8497bf8f07f80069ff76c448
dinhgiaandev <dinhgiaanforwork@gmail.com>
2024-09-06 18:22:02 +0700

    create navbar

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,19 +1,7 @@
 const connection = require('../config/database')
 
 const getHomepage = (req, res) => {
-    let users = [];
-
-    // connection.query(
-    //     'select * from Persons p',
-    //     function (err, results, fields) {
-    //         users = results;
-    //         console.log(">>> check results: ", results);
-
-    //         // console.log(">>> check users: ", users);
-    //         res.send(JSON.stringify(users))
-    //     }
-    // );
-    res.send("Homepage")
+    return res.render('home.ejs')
 }
 
 const getUsers = (req, res) => {
